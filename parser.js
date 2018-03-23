3e75d752988b570bc6b5ea0f374f4d40a7642edd
isidornygren <karlisidornygren@gmail.com>
2018-03-23 02:37:59 +0100

    Added parsing expressions for cout.

--- parser.js
+++ parser.js
@@ -1,17 +1,18 @@
 /**
  * Created by Rasmus on 2018-03-22.
  */
- const state = {name: 'null', stage: null};
+ const state = {name: 'null', stage: null, expression: ''};
  const memory = [];
 
  function clearState(){
    state.name = 'null';
    state.stage = null;
+   state.expression = '';
  }
 
 function parser (tokens) {
     var AST = {
-        type: 'function',
+        type: 'AST',
         body: []
     }
 
@@ -21,14 +22,12 @@ function parser (tokens) {
         var current_token = tokens.shift();
         // Since number token does not do anything by it self, we only analyze syntax when we find a word.
         if (current_token.type === 'word') {
-          console.log("Current token: ");
-          console.log(current_token);
             switch(state.name) {
               case 'function_name':
-                console.log("Creating new function: " + current_token.value);
                 var expression = {
                   type: 'function',
                   name: current_token.value,
+                  body: [], // Body consists of expressions
                   arguments: []
                 }
                 state.name = 'function_end'
@@ -38,6 +37,11 @@ function parser (tokens) {
                   if(current_token.value !== 'jah' && current_token.value !== 'rasta'){
                     state.stage.arguments.splice(state.stage.arguments.length, 0, current_token.value); // Inserts into last position of array
                   }
+                break;
+              case 'cout':
+                if(current_token.value !== 'Bless' && current_token.value !== 'up'){ // Semicolon check
+                  state.expression += current_token.value;
+                }
                 break;
             }
             switch (current_token.value) {
@@ -71,21 +75,47 @@ function parser (tokens) {
                     throw 'Wagwan Haile Selassie himself would not use the lords name in vain'
                   }
                   break;
+                case 'say':
+                  if(memory[0].value === 'I'){
+                      state.name = 'cout'
+                  }else{
+                    throw 'Praise be who did say?'
+                  }
+                  break;
+                case 'up':
+                  if(memory[0].value === 'Bless'){
+                      var expression = {
+                        type: 'cout',
+                        body: (JSON.parse(JSON.stringify(state.expression)))
+                      }
+                      state.expression = null;
+                      if(state.stage.type === 'function'){
+                        state.name = 'function_inside';
+                      }
+                      state.stage.body.splice(state.stage.body.length, 0, expression);
+                  }else{
+                    throw 'Where do I and I put my Blessings today?'
+                  }
+                  break;
                 case 'love':
                   if(memory[0].value === 'One'){
-                    if(state.name === 'function_inside'){
-                      AST.body.push(state.stage);
+                    if(state.stage !== null){ // TODO do some better checks here
+                      AST.body.push(JSON.parse(JSON.stringify(state.stage)));
                       clearState();
                     }
                   }else{
-                    console.log(memory[0].value);
                     throw 'You blodclot One love'
                   }
             }
         }
         memory.splice(0, 0, current_token);
     }
+    if(state.stage !== null){
+      AST.body.push(JSON.parse(JSON.stringify(state.stage)));
+      clearState();
+    }
     console.log("Jah we are done parsing the file now mon!");
     console.log(AST);
+    console.log(AST.body[0]);
     return AST
 }
