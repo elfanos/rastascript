ba2b6a4cc0b26ba2ecb5d2d4e9f8b30a1b7b63de
isidornygren <karlisidornygren@gmail.com>
2018-03-23 01:49:00 +0100

    Updated parser to read function arguments

--- parser.js
+++ parser.js
@@ -1,29 +1,78 @@
 /**
  * Created by Rasmus on 2018-03-22.
  */
+ const state = {name: 'null', stage: null};
+ const memory = [];
+
+ function clearState(){
+   state.name = 'null';
+   state.stage = null;
+ }
+
 function parser (tokens) {
     var AST = {
-        type: 'Drawing',
+        type: 'function',
         body: []
     }
+
     // extract a token at a time as current_token. Loop until we are out of tokens.
     while (tokens.length > 0){
         var current_token = tokens.shift();
-
         // Since number token does not do anything by it self, we only analyze syntax when we find a word.
         if (current_token.type === 'word') {
-            switch (current_token.value) {
-                case 'GiveThanksTo' :
-                    console.log("yolo inside pushish?");
-                    var expression = {
-                        type: 'CallExpression',
-                        name: 'GiveThanksTo',
-                        arguments: []
-                    };
-                    AST.body.push(expression);
+          console.log("Current token: ");
+          console.log(current_token);
+            switch(state.name) {
+              case 'function_name':
+                console.log("Creating new function: " + current_token.value);
+                var expression = {
+                  type: 'function',
+                  name: current_token.value,
+                  arguments: []
+                }
+                state.name = 'function_args'
+                state.stage = expression;
+                break;
+              case 'function_args':
+                  if(current_token.value !== 'jah'){
+                    state.stage.arguments.splice(state.stage.arguments.length, 0, current_token.value); // Inserts into last position of array
+                  }
                 break;
             }
+            switch (current_token.value) {
+                case 'Give':
+                    console.log("Give thanks");
+                  break;
+                case 'thanks': // Give thanks : function type, if to is followed, arguments are added
+                  if(memory[0].value === 'Give'){
+
+                  }else {
+                    throw 'Ya need to give thanks to Jah mon'
+                  }
+                  break;
+                case 'to': // If it follows a thanks then arguments are added
+                  if(memory[0].value === 'thanks'){
+                    console.log("New function creation started");
+                    state.name = 'function_name';
+                  }else {
+                    throw 'Who you giving thanks to mon'
+                  }
+                  break;
+                case 'rasta':
+                  if(memory[0].value === 'jah'){
+                    if(state.name === 'function_args'){
+                      console.log("New function has ended");
+                      AST.body.push(state.stage);
+                      clearState();
+                    }
+                  }
+                  break;
+            }
         }
+        //memory.insert(current_token);
+        memory.splice(0, 0, current_token);
     }
+    console.log("Jah we are done parsing the file now mon!");
+    console.log(AST);
     return AST
 }
